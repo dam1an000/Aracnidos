eaed9cb95a1600c32665c2c2788178d1434c0f93
dam1an000 <damianb071396@hotmail.com>
2023-03-02 13:55:18 -0600

    galeria 1.0
    agrege galeria incompleta

--- src/app/content/galeria/galeria.component.ts
+++ src/app/content/galeria/galeria.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CargarscriptsService } from 'src/app/cargarscripts.service';
 
 @Component({
   selector: 'app-galeria',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./galeria.component.css']
 })
 export class GaleriaComponent {
+  constructor(private cargarscripts: CargarscriptsService){
+    cargarscripts.carga([
 
+       "assets/vendor/aos/aos.js",
+       "assets/vendor/bootstrap/js/bootstrap.bundle.min.js",
+       "assets/vendor/glightbox/js/glightbox.min.js",
+       "assets/vendor/isotope-layout/isotope.pkgd.min.js",
+       "assets/vendor/swiper/swiper-bundle.min.js",
+       "assets/vendor/php-email-form/validate.js",
+       "assets/js/main.js"
+    ])
+  }
 }
