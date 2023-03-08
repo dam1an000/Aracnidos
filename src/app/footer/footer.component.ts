0df624732335f2e46e6297aa83cc9dff60e38fe3
dam1an000 <damianb071396@hotmail.com>
2023-03-08 13:47:26 -0600

    footer
    123

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import { CargarscriptsService } from '../cargarscripts.service';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+  constructor(private cargarscripts: CargarscriptsService){
+    cargarscripts.carga([
 
+       "assets/vendor/aos/aos.js",
+       "assets/vendor/bootstrap/js/bootstrap.bundle.min.js",
+       "assets/vendor/glightbox/js/glightbox.min.js",
+       "assets/vendor/isotope-layout/isotope.pkgd.min.js",
+       "assets/vendor/swiper/swiper-bundle.min.js",
+       "assets/vendor/php-email-form/validate.js",
+       "assets/js/main.js"
+    ])
+  }
 }
