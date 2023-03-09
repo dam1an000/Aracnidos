868e16da83f0a7572aa5b9f99cb33c43353e1ec5
dam1an000 <damianb071396@hotmail.com>
2023-03-09 00:03:01 -0600

    le puse cosas
    si

--- src/app/content/tarantulas/tarantulas.component.ts
+++ src/app/content/tarantulas/tarantulas.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CargarscriptsService } from 'src/app/cargarscripts.service';
 
 @Component({
   selector: 'app-tarantulas',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./tarantulas.component.css']
 })
 export class TarantulasComponent {
+  constructor(private cargarscripts: CargarscriptsService){
+    cargarscripts.carga([
 
+       "assets/vendor/aos/aos.js",
+       "assets/vendor/bootstrap/js/bootstrap.bundle.min.js",
+       "assets/vendor/glightbox/js/glightbox.min.js",
+       "assets/vendor/isotope-layout/isotope.pkgd.min.js",
+       "assets/vendor/swiper/swiper-bundle.min.js",
+       "assets/vendor/php-email-form/validate.js",
+       "assets/js/main.js"
+    ])
+  }
 }
