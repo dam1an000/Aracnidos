60276e66e2d7c557c37d986aa8b1223d18d07f7e
dam1an000 <damianb071396@hotmail.com>
2023-03-21 14:05:23 -0500

    metodos falta terminar
    no me dejaron acabar el trabajo

--- src/app/content/registro/registro.component.ts
+++ src/app/content/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth} from '@angular/fire/compat/auth'
 
 @Component({
   selector: 'app-registro',
@@ -7,4 +8,52 @@ import { Component } from '@angular/core';
 })
 export class RegistroComponent {
 
+  form={
+    nombre: "",
+    correo: "",
+    contra: "",
+  }
+  ccontra:any
+
+  constructor(private auth: AngularFireAuth){
+
+  }
+  ngOnInit(){
+     
+  }
+  registar(){
+    if (this.form.nombre != '' && this.form.correo != '' && this.form.contra != '' && this.ccontra != '') {
+      if (this.form.contra != this.ccontra) {
+        alert("Las contraseñas no coinciden")
+      }
+      else{
+        this.adduser(this.form).then((result)=>{
+          if (result) {
+            alert("¡Registrado correctamente!")
+            this.ngOnInit()
+          }
+          else{
+            alert("Error. intente nuevamente.")
+          }
+        })
+      }
+    }
+    else{
+      alert("Favor de completar los datos")
+    }
+  }
+  adduser(form:any){
+    var promise = new Promise((resolve,reject)=>{
+      this.auth.createUserWithEmailAndPassword(form.correo,form.contra).then((result)=>{
+        resolve(result)
+      })
+      .catch(function(error){
+        if (error.code == "auth/invalid-email") {
+          alert("Ingresa un correo electro") 
+          
+        }
+      })
+    })
+
+  }
 }
